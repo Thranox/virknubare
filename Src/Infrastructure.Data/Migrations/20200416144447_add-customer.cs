24439ca9b0a523a855faf2f4e4a0c00145226772
Anders Juul <andersjuulsfirma@gmail.com>
2020-04-16 16:46:19 +0200

    added migrations

---
+++ Src/Infrastructure.Data/Migrations/20200416144447_add-customer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class addcustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinalized",
+                table: "TravelExpenses",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Stage",
+                table: "TravelExpenses",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "CustomerEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerEntities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlowStepEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Key = table.Column<string>(nullable: true),
+                    From = table.Column<int>(nullable: false),
+                    CustomerEntityId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlowStepEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FlowStepEntity_CustomerEntities_CustomerEntityId",
+                        column: x => x.CustomerEntityId,
+                        principalTable: "CustomerEntities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CustomerId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserEntity_CustomerEntities_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "CustomerEntities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlowStepEntity_CustomerEntityId",
+                table: "FlowStepEntity",
+                column: "CustomerEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserEntity_CustomerId",
+                table: "UserEntity",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlowStepEntity");
+
+            migrationBuilder.DropTable(
+                name: "UserEntity");
+
+            migrationBuilder.DropTable(
+                name: "CustomerEntities");
+
+            migrationBuilder.DropColumn(
+                name: "IsFinalized",
+                table: "TravelExpenses");
+
+            migrationBuilder.DropColumn(
+                name: "Stage",
+                table: "TravelExpenses");
+        }
+    }
+}
