a3e15aef38d446125d0dd299b240536da02a0ada
Anders Juul <andersjuulsfirma@gmail.com>
2020-04-23 20:45:21 +0200

    Added HealthController

---
+++ Src/IDP/HealthController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IDP
+{
+    [AllowAnonymous]
+    [SecurityHeaders]
+    public class HealthController : Controller
+    {
+        private readonly ILogger<HealthController> _logger;
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ShallowCheck()
+        {
+            _logger.LogInformation("Health Shallow Check!");
+
+            return await Task.FromResult( Ok());
+        }
+    }
+}
