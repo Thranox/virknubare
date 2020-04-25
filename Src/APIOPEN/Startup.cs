bb1476e1a18d1d04caeb2d66b7c0513f274c66bc
Anders Juul <andersjuulsfirma@gmail.com>
2020-04-25 23:58:14 +0200

    Pushing build

--- Src/APIOPEN/Startup.cs
+++ Src/APIOPEN/Startup.cs
@@ -31,8 +31,8 @@ namespace APIOPEN
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.AddPolApi(_configuration, false);
+            services.AddControllers();
 
             services.AddDbContext<PolDbContext>(options =>
             {
