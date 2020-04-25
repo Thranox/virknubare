f656969751d87574b9c458c3bf9107f0357c6e82
Anders Juul <andersjuulsfirma@gmail.com>
2020-04-25 07:40:11 +0200

    Added version info til Ids4 pages

---
+++ Src/SharedWouldBeNugets/BuildVersionInfoProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SharedWouldBeNugets
+{
+    public static class BuildVersionInfoProvider
+    {
+        public static string GetBuildVersionInfoString(Assembly assembly)
+        {
+            var assemblyFullName = assembly.FullName.Split(",").First();
+            var versionTime = File.GetLastWriteTime(assembly.Location);
+            return assemblyFullName + " " +versionTime;
+        }
+    }
+}
