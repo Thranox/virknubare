86de3627c2714ef35913d80089e914cf59f2c7a2
Anders Juul <andersjuulsfirma@gmail.com>
2020-05-05 17:44:50 +0200

    confgi changes

--- Src/Web/ClientApp/src/app/constants.ts
+++ Src/Web/ClientApp/src/app/constants.ts
@@ -2,8 +2,8 @@
 export class Constants {
   public static clientRoot = 'https://localhost:44324/';
 
-  //public static apiRoot = 'https://localhost:44324/';
-  //public static apiRoot = 'https://localhost:44348/';
+  //public static apiRoot = 'https://localhost:44324/'; //PolApi via Web
+  //public static apiRoot = 'https://localhost:44348/'; //PolApi
   public static apiRoot = 'https://dev.politikerafregning.dk/polapi/';
   //public static apiRoot = 'https://localhost:44396/';
   //public static apiRoot = 'https://andersathome.dk/polapi/';
@@ -16,8 +16,8 @@ export class Constants {
   //public static stsAuthority = 'http://ajf-prod-02/IDP/';
   //public static stsAuthority = 'https://andersathome.dk/IDP/';
   //public static stsAuthority = 'http://localhost:5000/';
-  //public static stsAuthority = 'https://localhost:5001/';
-  public static stsAuthority = 'https://dev.politikerafregning.dk/IDP/';
+  public static stsAuthority = 'https://localhost:5001/';
+  //public static stsAuthority = 'https://dev.politikerafregning.dk/IDP/';
 
   public static clientId = 'polangularclient';
 }
