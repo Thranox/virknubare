59137f6790a027322cfd0ece2e1ab8b75b60dd9a
Andreas Lysdal Andersen <ala@3mand.dk>
2020-05-20 09:07:27 +0200

    Update login state on app start

--- Src/Web/App/src/app/app.component.ts
+++ Src/Web/App/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./core/auth-service.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App';
+  private isLoggedIn: boolean;
+
+  constructor(private authService: AuthService) {
+    this.authService.isLoggedIn().then(loggedIn => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
 }
--- Src/Web/App/src/app/modules/page2/page2.component.ts
+++ Src/Web/App/src/app/modules/page2/page2.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../core/auth-service.component';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Constants} from '../../constants';
-import {environment} from "../../../environments/environment";
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-page2',
