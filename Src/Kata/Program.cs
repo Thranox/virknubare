d0ccd5f53706ad802a01d8ecb2602c1bd1543ec3
Anders Juul <andersjuulsfirma@gmail.com>
2020-05-27 19:52:22 +0200

    Added Kata step

--- Src/Kata/Program.cs
+++ Src/Kata/Program.cs
@@ -62,7 +62,7 @@ namespace Kata
 
                     new KataStepDescriptor("ResetTestData").AsUser("alice").WithVerification(c=>c.DatabaseResetResponse!=null),
 
-                    new KataStepDescriptor("GetUserInfo").AsUser("freddie").WithVerification(c=>c.UserInfoGetResponse!=null),
+                    new KataStepDescriptor("GetUserInfo").AsUser("freddie").WithVerification(c=>c.UserInfoGetResponse!=null && c.UserInfoGetResponse.UserCustomerInfo.All(x=>x.UserCustomerStatus!=0)),
 
                     new KataStepDescriptor("GetUserInfo").AsUser("alice").WithVerification(c=>c.UserInfoGetResponse!=null),
                     new KataStepDescriptor("GetAllTravelExpenses").AsUser("alice").WithVerification(c=>c.TravelExpenseGetResponse?.Result!=null && c.TravelExpenseGetResponse.Result.Count()==TestData.GetNumberOfTestDataTravelExpenses()),
