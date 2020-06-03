43331e69e49ff1b590f8b2cca2b35a8cd4407316
Tobias Tjørnelund Jensen <ttj@improvento.com>
2020-06-03 15:13:47 +0200

    delete commented code

--- Src/Tests/Web/MailSendingTests.cs
+++ Src/Tests/Web/MailSendingTests.cs
@@ -11,7 +11,6 @@ namespace Tests.Web
     public class MailSendingTests
     {
         [Test]
-        //[Explicit]
         public async Task SendEmail()
         {
 
@@ -19,7 +18,7 @@ namespace Tests.Web
             {
                 smtp.Host = "172.20.157.15";
                 smtp.Port = 25;
-                smtp.EnableSsl = false;
+                smtp.EnableSsl = true;
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
                 smtp.Credentials = new NetworkCredential("", "");
                 smtp.Timeout = 100000;
@@ -29,45 +28,6 @@ namespace Tests.Web
             var fromAddress = "tobias.jensen@improvento.com";
             var toAddress = "tobias.jensen@improvento.com";
             await smtp.SendMailAsync(fromAddress, toAddress, subject, body);
-            
-            /*
-            // Note: This API Key for sendgrid is for dev & test only. If we use sendgrid for production we need an improvento account.
-            var client = new SendGridClient("SG"+
-                                            "."+
-                                            "yNWsGff6QIK9Ob1hQ0_EGw"+
-                                            "."+
-                                            "TqTZWHYI1cb6dktFe3S19FdA8"+ 
-                                            "Y" + 
-                                            "NLjPsyQixXY_37VAY");
-
-            // Send a Single Email using the Mail Helper
-            var from = new EmailAddress("andersjuulsfirma@gmail.com", "Anders Juul");
-            //var subject = "Hello World from the Twilio SendGrid CSharp Library Helper!";
-            var to = new EmailAddress("andersjuulsfirma@gmail.com", "Anders Juul");
-            var plainTextContent = "Hello, Email from the helper [SendSingleEmailAsync]!";
-            var htmlContent = "<strong>Hello, Email from the helper! [SendSingleEmailAsync]</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            Response response = await client.SendEmailAsync(msg);
-            Console.WriteLine(msg.Serialize());
-            Console.WriteLine(response.StatusCode);
-            Console.WriteLine(response.Headers);
-
-            // Send a Single Email using the Mail Helper with convenience methods and initialized SendGridMessage object
-            msg = new SendGridMessage()
-            {
-                From = new EmailAddress("andersjuulsfirma@gmail.com", "Anders Juul"),
-                Subject = "Hello World 22 from the Twilio SendGrid CSharp Library Helper!",
-                PlainTextContent = "Hello, Email from the helper [SendSingleEmailAsync]!",
-                HtmlContent = "<strong>Hello, Email from the helper! [SendSingleEmailAsync]</strong>"
-            };
-            msg.AddTo(new EmailAddress("andersjuulsfirma@gmail.com", "Anders Juul"));
-
-            response = await client.SendEmailAsync(msg);
-            Console.WriteLine(msg.Serialize());
-            Console.WriteLine(response.StatusCode);
-            Console.WriteLine(response.Headers);
-            Console.WriteLine("\n\nPress <Enter> to continue.");
-            */
         }
     }
 }
