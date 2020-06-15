345529db06d2d07d5c3518704c62eadc49d3a29c
Anders Juul <andersjuulsfirma@gmail.com>
2020-06-15 11:35:29 +0200

    Added auto mail sending (for testing purposes) every time IDP starts

--- Src/IDP/Startup.cs
+++ Src/IDP/Startup.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4.Services;
+﻿using System;
+using IdentityServer4.Services;
 using IdentityServerAspNetIdentit.Data;
 using IdentityServerAspNetIdentit.Models;
 using Microsoft.AspNetCore.Builder;
@@ -147,6 +148,21 @@ namespace IDP
             {
                 endpoints.MapDefaultControllerRoute();
             });
+
+            try
+            {
+                var mailService = app
+                    .ApplicationServices
+                    .CreateScope()
+                    .ServiceProvider
+                    .GetRequiredService<IMailService>();
+                mailService.SendAsync("andersjuulsfirma@gmail.com", "andersjuulsfirma@gmail.com", "IDP is up",
+                    "Empty body!").Wait(TimeSpan.FromSeconds(3));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
